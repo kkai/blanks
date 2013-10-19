ff99f8533041184ebbff308fe184cf9126273531
Kai Kunze <kaikunze@icloud.com>
2013-10-19 21:46:00 +0900

    add MoreBlanks target

---
+++ MoreBlanks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoreBlanks
+//
+//  Created by Kai Kunze on 19/10/2013.
+//  Copyright (c) 2013 Kai Kunze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoreBlanks/MainViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainViewController.m
+//  MoreBlanks
+//
+//  Created by Kai Kunze on 19/10/2013.
+//  Copyright (c) 2013 Kai Kunze. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Flipside View
+
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showAlternate"]) {
+        [[segue destinationViewController] setDelegate:self];
+    }
+}
+
+@end
