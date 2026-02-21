1d1943416fe3b2c5a6a18734210966a6d4a4363b
Kai Kunze <kaikunze@me.com>
2026-02-21 18:59:40 +0900

    Initial Commit

---
+++ blanks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blanks
+//
+//  Created by kai on 21.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blanks/blanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  blanksApp.swift
+//  blanks
+//
+//  Created by kai on 21.02.26.
+//
+
+import SwiftUI
+
+@main
+struct blanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
